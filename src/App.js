bd2c661059eca7e1e852b002e7ca503cd0515d53
Eduarda Almeida <eduardapalm27@gmail.com>
2021-09-06 22:04:48 -0300

    Create:  get method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { api } from "./services/Weather-Api"
 
 function App() {
+
+  async function handleGetWeather(){
+    const response = await api.get("Curitiba")
+    console.log(response)
+  }
+
   return (
     <>
+      <button onClick={handleGetWeather}>Search Weather</button>
     </>
   );
 }
