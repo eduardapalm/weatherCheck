44cd903c18a2a47c6cab0620b7ec1e250738d51a
Eduarda Almeida <eduardapalm27@gmail.com>
2021-09-07 14:13:10 -0300

    Update: showing the api informations on screen

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import React, { useState } from 'react'
-import { api } from "./services/Weather-Api"
+import React, { useState, useEffect } from "react";
+import { api } from "./services/Weather-Api";
 
 function App() {
+  const [weather, setWeather] = useState();
+  const city = "Sergipe";
 
-  const [weather, setWeather] = useState()
-  const city = "Rio de Janeiro"
-
-  async function handleGetWeather(){
-    const response = await api.get(city)
-    console.log(response.data)
-    setWeather(response.data)
+  async function handleGetWeather() {
+    const response = await api.get(city);
+    console.log(response.data);
+    setWeather(response.data);
   }
 
   return (
     <>
+      {/* <header>
+        <button onClick={handleGetWeather}>Search Weather</button>
+      </header> */}
 
-    <header>
-      <button onClick={handleGetWeather}>Buscar Previsão</button>
-    </header>
+      {weather && (
+        <main>
+          <h1>{city}</h1>
 
-    <main>
-      {JSON.stringify(weather)}
-    </main>
+          <section>
+            <h2>Current Weather:</h2>
+            <p>{weather.temperature}</p>
+            <p>{weather.description}</p>
+            <p>Wind speed: {weather.wind}</p>
+          </section>
 
-
+          <section>
+            <h2>Forecast:</h2>
+            {
+              weather.forecast.map( day =>
+                <>
+                  <p>{day.temperature}</p>
+                  <p>{day.wind}</p>
+                </>
+              )
+            }
+          </section>
+        </main>
+      )}
     </>
   );
 }
